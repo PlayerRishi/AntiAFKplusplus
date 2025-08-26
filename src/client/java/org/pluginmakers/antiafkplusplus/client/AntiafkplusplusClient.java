3f3b15701e5d2f488d275f74deba6c369fcc195a
AR <aryanrishi23@gmail.com>
2025-08-26 15:58:29 -0400

    Project Creation!

---
+++ src/client/java/org/pluginmakers/antiafkplusplus/client/AntiafkplusplusClient.java
@@ -0,0 +1,10 @@
+package org.pluginmakers.antiafkplusplus.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AntiafkplusplusClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/org/pluginmakers/antiafkplusplus/Antiafkplusplus.java
@@ -0,0 +1,10 @@
+package org.pluginmakers.antiafkplusplus;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Antiafkplusplus implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
